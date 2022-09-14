93efe3f40f8fd04617004b9e8b460e2763776fb4
vijaypurushoth06 <111448427+vijaypurushoth06@users.noreply.github.com>
2022-09-14 22:27:21 +0530

    Ex4.1.java

--- Ex4.2.java
+++ Ex4.2.java
@@ -1,65 +1,76 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package ex4;
+package Abstract_class_and_Interface;
 
-/**
- *
- * @author 21cse125
- */
-interface RegularPolygon
-{
- abstract int getNumSides();
- abstract int getSideLength();
- default void getPerimeter()
- {
-     
- }
- }
+import java.util.Scanner;
 
-class EquilateralTriangle implements RegularPolygon
-{
-    public int fun() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-    int sides;
-    int sid = 3;
-   EquilateralTriangle(int sides)
-   {
-          this.sides = sides;      
-   }
-    public int getNumSides()
-    {
-     return sid;  
-    }
-    public int getSideLength()
-    {
-        return sides;
-    }
+import java.util.Scanner;
+
+abstract class Book {
+    String title = null, author = null;
+    public int price = 0;
+
+    abstract void setdetails();
+
+    abstract String gettitle();
 }
-class Square implements RegularPolygon
-{
- int sides;
- int sid = 4;
- Square (int sides)
- {
-  this.sides= sides;  
- }
- public int getNumSides()
- {
-  return sid;  
- }
- public int getSideLength()
- {
-  return sides;  
- }
+
+class Mybook extends Book {
+
+    void setdetails() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter book details");
+        System.out.println("Enter title : ");
+        super.title = scan.next();
+        System.out.println("Enter author name : ");
+        super.author = scan.next();
+        System.out.println("Enter price of book ");
+        price = scan.nextInt();
+    }
+
+    String gettitle() {
+        return super.title;
+    }
+
 }
-public class Ex4_2 {
- 
-  public static void main(String args[])
-  {
-     
-     
-  }
+
+public class Ex4_1 {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int nos, temp, t1;
+        System.out.println("Enter the number of book objects :");
+        nos = scan.nextInt();
+        Mybook arr[] = new Mybook[nos];
+        for (int x = 0; x < nos; x++) {
+            arr[x] = new Mybook();
+        }
+        while (true) {
+            System.out.println("1.Enter data\n2.Get title\n3.Get Total price");
+            temp = scan.nextInt();
+            try {
+                switch (temp) {
+                    case 1:
+
+                        System.out.println("Enter book number : ");
+                        t1 = scan.nextInt();
+                        arr[t1 - 1].setdetails();
+                        break;
+                    case 2:
+                        System.out.println("Enter Book number : ");
+                        t1 = scan.nextInt();
+                        System.out.println(arr[t1 - 1].gettitle());
+                        break;
+                    case 3:
+                        int tot = 0;
+                        for (Mybook x : arr) {
+                            tot += x.price;
+                        }
+                        System.out.println("Total prices of all books is : " + tot);
+                        break;
+                    default:
+                        System.out.println("Enter a valid option");
+                }
+            } catch (ArrayIndexOutOfBoundsException obj) {
+                System.out.println("Please Enter a book number within specified range");
+            }
+        }
+    }
 }
